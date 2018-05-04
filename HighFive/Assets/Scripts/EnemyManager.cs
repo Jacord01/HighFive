ff9f899009f4814fcabbbeb77bb7b29198f8440c
Javier Cordero Calvo <jacord01@ucm.es>
2018-05-04 21:05:50 +0200

    Añadido dropeo lanchas

---
+++ HighFive/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+    public int lives = 10;
+
+    [SerializeField]
+    public Transform[] wheres;
+
+    //public Transform whereToDrop1;
+    //public Transform whereToDrop2;
+    //public Transform whereToDrop3;
+    //public Transform whereToDrop4;
+
+    public GameObject DropElement = null;
+
+    private void Start()
+    {
+        drop();
+    }
+
+    // Update is called once per frame
+    void Update () {
+      
+	}
+
+    void drop()
+    {
+       
+        int p = Random.Range(0, 4);
+        Debug.Log(p);
+
+        for (int i = 0; i < p; i++)
+        {
+            Instantiate(DropElement, wheres[i]);
+
+        }
+
+    }
+}
