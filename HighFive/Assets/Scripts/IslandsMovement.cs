793dcbd6f8a87ebea198d9300baeb0320061128d
Javier Cordero Calvo <jacord01@ucm.es>
2018-05-18 20:34:07 +0200

    soy tonto

--- HighFive/Assets/Scripts/IslandsMovement.cs
+++ HighFive/Assets/Scripts/IslandsMovement.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class IslandsMovement : MonoBehaviour {
 
+    bool paused = false;
 	// Update is called once per frame
 	void Update () {
+        if(!paused)
         this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 0.1f / 10.5f * Mathf.Sin(Time.time * 2)
           , this.transform.position.z);
+    }
+
+   public void setPause(bool p)
+    {
+        paused = p;
     }
 }
--- HighFive/Assets/Scripts/ShipMov.cs
+++ HighFive/Assets/Scripts/ShipMov.cs
@@ -7,26 +7,57 @@ public class ShipMov : MonoBehaviour {
     public float health;
     public GameObject StandPoint1;
     public GameObject StandPoint2;
-    public float speed = 10;
+    float speed = 0;
+    public float maxSpeed = 10;
+    public float velocity = 0.1f;
     bool StandPoint1Occupied_ = false;
     bool StandPoint2Occupied_ = false;
 
+    Rigidbody rb;
+
     // Use this for initialization
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update () {
 
         if ((Input.GetKey(KeyCode.W)))
-            this.transform.Translate(0, -speed * Time.deltaTime,0 ) ;
+        {
+            speed += velocity / 2;
+          
+        }
         else
-            this.transform.Translate(0, 0, 0);
+        {
+            speed -= velocity / 1.5f;
+        }
+
+        if (speed > maxSpeed)
+            speed = maxSpeed;
+        else if (speed < 0)
+            speed = 0;
+
+        transform.Translate(0, -speed * Time.deltaTime, 0);
+
 
         if (Input.GetKey(KeyCode.A))
-            this.transform.Rotate(0, 0, -30 * Time.deltaTime);
-        else if (Input.GetKey(KeyCode.D))
-            this.transform.Rotate(0, 0, 30 * Time.deltaTime);
+        {
 
-    }
+            rb.AddTorque(transform.forward * 10);
+            transform.Rotate(0, 0, -30 * Time.deltaTime);
+            
+        }
+
+     
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 0, 30 * Time.deltaTime);
+        }
+
+      }
 
     public  GameObject getSP1()
     {
